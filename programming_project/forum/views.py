1d289af557658c3353812f131a8be3f674cd2464
Ioann <ioan@mail.cru>
2022-04-04 20:23:56 +0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	programming_project/db.sqlite3
    #	programming_project/forum/admin.py
    #	programming_project/forum/templates/search_results.html
    #	programming_project/forum/views.py

--- programming_project/forum/views.py
+++ programming_project/forum/views.py
@@ -1,51 +1,29 @@
 from django.db.models import Q
-<<<<<<<<< Temporary merge branch 1
-from django.shortcuts import render
-from django.views import View
-from django.views.generic import ListView
 from .models import Topic
-=========
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.views.generic import ListView
 from taggit.models import Tag
->>>>>>>>> Temporary merge branch 2
 
-from .models import Topic
-
-<<<<<<<<< Temporary merge branch 1
-def Main(request):
-    topics = Topic.objects.all().order_by('name')
-    return render(request, 'index.html', {'topics': topics})
-
-=========
 
 def Main(request):
     topics = Topic.objects.all().order_by('name')
-    return render(request, 'index.html', {'topics': topics})
+    return render(request, 'index.html', {{'topics': topics}})
 
->>>>>>>>> Temporary merge branch 2
 
 class Search(ListView):
     model = Topic
     template_name = 'search_results.html'
 
-<<<<<<<<< Temporary merge branch 1
-    def get_queryset(self):  # новый
-=========
     def get_queryset(self):
->>>>>>>>> Temporary merge branch 2
         query = self.request.GET.get('q')
         object_list = Topic.objects.filter(
             Q(name__icontains=query) | Q(content__icontains=query)
         )
-<<<<<<<<< Temporary merge branch 1
-        return object_list
-=========
-
         return object_list
 
-    def post_list(request, tag_slug=None):
+
+    def topic_list(request, tag_slug=None):
         object_list = Topic.published.all()
         topics = Topic.objects.all().order_by('name')
 
@@ -61,8 +39,10 @@ class Search(ListView):
                        'tag': tag})
 
 
+
 class SingleTopicPageView(View):
     template_name = 'single-topic.html'
 
     def get(self, request):
         return render(request, self.template_name)
+
