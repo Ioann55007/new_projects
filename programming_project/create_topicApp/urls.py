d3b4912b99841b7ac088eebb4e88aeeba2d20199
ioann <ioann.basic@gmail.com>
2022-06-19 09:43:29 +0500

    registration

--- programming_project/create_topicApp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-app_name = 'create_topicApp'
-
-urlpatterns = [
-    path('create_top/', views.CreateTopicView.as_view(), name='create_top'),
-]
--- programming_project/create_topicApp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views import View
-
-
-class CreateTopicView(View):
-    template_name = 'create_top.html'
-
-    def get(self, request):
-        return render(request, 'create_top.html')
