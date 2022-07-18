75e386ed93c8b8c936b2b9866f3814106314aacc
ioann <ioann.basic@gmail.com>
2022-07-18 18:32:23 +0500

    user profile

--- programming_project/registration_App/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views import View
-
-
-class SimpleSignupView(View):
-    template_name = 'simple-signup.html'
-
-    def get(self, request):
-        return render(request, self.template_name)
